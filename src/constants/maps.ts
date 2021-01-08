7d03473507e7b3831fc6c9b56b215b0c66743c30
Rokoucha <git@rokoucha.net>
2021-01-08 17:38:59 +0900

    fix: Map url

--- src/constants/maps.ts
+++ src/constants/maps.ts
@@ -2,46 +2,46 @@ export const MAPS: { name: string; url: string }[] = [
   {
     name: 'Factory',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/8/83/Factory_0.8.png/revision/latest?cb=20180506020439',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/8/83/Factory_0.8.png',
   },
   {
     name: 'Customs',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/5/55/CustomsLargeExpansionGloryMonki.png/revision/latest?cb=20200805222908',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/5/55/CustomsLargeExpansionGloryMonki.png',
   },
   {
     name: 'Woods',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/05/Glory4lyfeWoods_map_v4_marked.png/revision/latest?cb=20210103091918',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/05/Glory4lyfeWoods_map_v4_marked.png',
   },
   {
     name: 'Shoreline',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/a/ac/Shoreline_Map_-_12.7.png/revision/latest?cb=20200809112827',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/a/ac/Shoreline_Map_-_12.7.png',
   },
   {
     name: 'Interchange',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/e/e5/InterchangeMap_Updated_4.24.2020.png/revision/latest?cb=20200424115934',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/e/e5/InterchangeMap_Updated_4.24.2020.png',
   },
   {
     name: 'The Lab: Basement',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/06/TheLabBasementByLogiwonk.png/revision/latest?cb=20190210195439',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/06/TheLabBasementByLogiwonk.png',
   },
   {
     name: 'The Lab: First floor',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/3/35/TheLabFirstFloorByLogiwonk.png/revision/latest?cb=20190210195441',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/3/35/TheLabFirstFloorByLogiwonk.png',
   },
   {
     name: 'The Lab: Second floor',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/0f/TheLabSecondFloorByLogiwonk.png/revision/latest?cb=20190210195443',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/0f/TheLabSecondFloorByLogiwonk.png',
   },
   {
     name: 'Reserve',
     url:
-      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/4/42/3D_Map_by_loweffortsaltbox.png/revision/latest?cb=20200410160036',
+      'https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/4/42/3D_Map_by_loweffortsaltbox.png',
   },
 ]
